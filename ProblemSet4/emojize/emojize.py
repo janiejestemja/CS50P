5c7884780fc4c8d383d20896f56e113ccadb831b
janiejestemja <janiejestemja@web.de>
2024-12-20 16:56:18 +0100

    Fixed emojize.

--- ProblemSet4/emojize/emojize.py
+++ ProblemSet4/emojize/emojize.py
@@ -4,7 +4,7 @@ import emoji
 
 def main():
 	# using emoji per emoji documentation passing user input directly as an argument
-	print("Output:", emoji.emojize(input("Input: ")))
+	print("Output:", emoji.emojize(input("Input: "), language="alias"))
 
 if __name__ == "__main__":
 	main()
